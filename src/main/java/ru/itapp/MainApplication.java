f7f14f4affa2d384e729f52abba18e9a5be8c4a3
Daniil <myps4accaunt2001@gmail.com>
2022-01-06 01:10:07 +0300

    add some prop

--- src/main/java/ru/itapp/MainApplication.java
+++ src/main/java/ru/itapp/MainApplication.java
@@ -1,11 +1,21 @@
 package ru.itapp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 @SpringBootApplication
 public class MainApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
